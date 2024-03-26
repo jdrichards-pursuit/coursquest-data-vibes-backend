cfc07e0f36fc19c82d0eda59bc25dd18ca46ae8a
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-03-26 10:10:10 -0400

    reseeded course urls with higher resolution photos

--- db/seed.sql
+++ db/seed.sql
@@ -12,14 +12,14 @@ VALUES
 -- Inserting course data
 INSERT INTO courses (user_id, name, subject, description, professor, image_url, start_date, end_date, difficulty, cost)
 VALUES 
-(1, 'Introduction to Programming', 'Computer Science', 'Learn the basics of programming.', 'Dr. Smith', 'https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,w_200,h_200/v1711049845/images_x0nve1.jpg', '2024-01-01', '2024-03-01', 'Beginner', 0.00),
-(1, 'Data Science Fundamentals', 'Data Science', 'Explore the world of data science.', 'Prof. Johnson', 'https://res.cloudinary.com/dluexpyt4/image/upload/v1711049998/images_msnikf.jpg', '2024-02-01', '2024-04-01', 'Intermediate', 29.99),
-(NULL, 'Web Development Fundamentals', 'Computer Science', 'Introduction to web development concepts.', 'Dr. Brown', 'https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,w_200,h_200/v1711049845/images_x0nve1.jpg', '2024-03-01', '2024-05-01', 'Beginner', 0.00),
-(NULL, 'Machine Learning Basics', 'Machine Learning', 'An introductory course to machine learning.', 'Prof. Lee', 'https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,w_200,h_200/v1711049745/machine_learning_umt76d.jpg', '2024-04-01', '2024-06-01', 'Intermediate', 49.99),
-(NULL, 'Digital Marketing Essentials', 'Marketing', 'Fundamentals of digital marketing strategies.', 'Dr. White', 'https://res.cloudinary.com/dluexpyt4/image/upload/v1711049998/images_msnikf.jpg', '2024-05-01', '2024-07-01', 'Beginner', 19.99),
-(1, 'Introduction to Drawing', 'Art', 'Learn the basics of drawing.', 'Prof. Davis', 'https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,w_200,h_200/v1711050617/pie5jenjmkftcn01zkaj.jpg', '2024-06-01', '2024-08-01', 'Beginner', 0.00),
-(1, 'Art History', 'Art', 'Explore the history of art from ancient times to the present.', 'Dr. Rodriguez', 'https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,w_200,h_200/v1711050617/pie5jenjmkftcn01zkaj.jpg', '2024-07-01', '2024-09-01', 'Intermediate', 39.99),
-(1, 'Photography Basics', 'Art', 'Introduction to the basics of photography.', 'Prof. Martinez', 'https://res.cloudinary.com/dluexpyt4/image/upload/c_crop,w_200,h_200/v1711050617/pie5jenjmkftcn01zkaj.jpg', '2024-08-01', '2024-10-01', 'Beginner', 29.99);
+(1, 'Introduction to Programming', 'Computer Science', 'Learn the basics of programming.', 'Dr. Smith', 'https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1711416478/CourseQuest%20-%20App/circuit_board_pic_kgsrqb.jpg', '2024-01-01', '2024-03-01', 'Beginner', 0.00),
+(1, 'Data Science Fundamentals', 'Data Science', 'Explore the world of data science.', 'Prof. Johnson', 'https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1711416940/CourseQuest%20-%20App/data-plot-image-jpg_bcv5io.jpg', '2024-02-01', '2024-04-01', 'Intermediate', 29.99),
+(NULL, 'Web Development Fundamentals', 'Computer Science', 'Introduction to web development concepts.', 'Dr. Brown', 'https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1711415511/CourseQuest%20-%20App/coding-svg_cf0sgo.jpg', '2024-03-01', '2024-05-01', 'Beginner', 0.00),
+(NULL, 'Machine Learning Basics', 'Machine Learning', 'An introductory course to machine learning.', 'Prof. Lee', 'https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1711418088/CourseQuest%20-%20App/network-image-jpg_ayubmd.jpg', '2024-04-01', '2024-06-01', 'Intermediate', 49.99),
+(NULL, 'Digital Marketing Essentials', 'Marketing', 'Fundamentals of digital marketing strategies.', 'Dr. White', 'https://res.cloudinary.com/dvmczcg3f/image/upload/c_crop,ar_16:9/v1711418541/CourseQuest%20-%20App/marketing-strateg-pic-jpg_pgzkzi.jpg', '2024-05-01', '2024-07-01', 'Beginner', 19.99),
+(1, 'Introduction to Drawing', 'Art', 'Learn the basics of drawing.', 'Prof. Davis', 'https://res.cloudinary.com/dvmczcg3f/image/upload/t_crop/v1711418337/CourseQuest%20-%20App/drawing-of-hands-jpg_kgiyip.jpg', '2024-06-01', '2024-08-01', 'Beginner', 0.00),
+(1, 'Art History', 'Art', 'Explore the history of art from ancient times to the present.', 'Dr. Rodriguez', 'https://res.cloudinary.com/dvmczcg3f/image/upload/c_crop,ar_16:9/v1711418248/CourseQuest%20-%20App/weaving-cloth-pic-jpg_zauofc.jpg', '2024-07-01', '2024-09-01', 'Intermediate', 39.99),
+(1, 'Photography Basics', 'Art', 'Introduction to the basics of photography.', 'Prof. Martinez', 'https://res.cloudinary.com/dvmczcg3f/image/upload/c_crop,ar_16:9/v1711416707/CourseQuest%20-%20App/photo-of-films-jpeg_dmk0xf.jpg', '2024-08-01', '2024-10-01', 'Beginner', 29.99);
 
 -- Inserting reviews data
 INSERT INTO reviews (user_id, course_id, rating, review)
