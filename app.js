add6a59d5094a30646212221b99dc808a97d1a6b
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2024-03-28 15:51:14 -0400

    For Render

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const cron = require('node-cron')
 
 const app = express();
 
@@ -13,6 +14,16 @@ app.use(express.json());
 app.use("/api/courses", courseController);
 app.use("/api/reviews", reviewController);
 app.use("/api/users", userController);
+
+app.use(cors())
+app.use(express.json())
+
+cron.schedule('*/10 * * * *', () => {
+  const currentTime = new Date().toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+  })
+  console.log(`Running a task every 10 minutes. Current time: ${currentTime}`)
+})
 
 // ROUTES
 app.get("/", (req, res) => {
